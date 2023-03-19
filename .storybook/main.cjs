3fc66b37555c1f685d77e86dd4373a97779c475f
Akihiro Tojo <akihiro.021109@gmail.com>
2023-03-19 23:52:20 +0900

    Update .eslintignore

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,19 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true,
   },
   async viteFinal(config, { configType }) {
     if (configType === 'PRODUCTION') {
@@ -22,4 +19,4 @@ module.exports = {
 
     return config;
   },
-}
+};
